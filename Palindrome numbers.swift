3fd6789a616a617589778fe45dc2dd662d707578
SurfSpotter <55549136+SurfSpotter@users.noreply.github.com>
2022-06-14 23:40:18 +0300

    Add files via upload

---
+++ Palindrome numbers.swift
@@ -0,0 +1,45 @@
+//
+//  Palindrome numbers.swift
+//  
+//
+//  Created by Алексей Чигарских on 14.06.2022.
+//
+
+//Palindrome numbers
+// input: 14541 output: - true
+// input: 14531 output: - false
+class Solution {
+    
+    func isPalindromeFirstSolution(_ x: Int) -> Bool {
+        let str = Array(String(x))
+        var start = 0
+        var end = str.count - 1
+        while start < end {
+            if str[start] == str[end] {
+                start += 1
+                end -= 1
+            } else {
+                return false
+            }
+        }
+        return true
+}
+    
+    
+    func isPalindromeSecondSolution(_ x: Int) -> Bool {
+        //Convert to array
+        let normal = Array(String(x))
+        let reversed = Array(String(x).reversed()) //Reverse array
+        
+        //Make two slices
+        let leftSlice = normal.prefix(normal.count/2)
+        let rightSlice = reversed.prefix(reversed.count/2)
+        
+        //Compare
+        if leftSlice == rightSlice {
+            return true
+        } else {
+            return false
+        }
+    }
+}
